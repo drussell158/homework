2b91792d8c7bf11036c7780213add663b6af1ee9
Dalton Russell <russell.158@wright.edu>
2016-09-07 23:06:50 -0400

    Add HelloWorld files to repo

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 	public static void main(String args[]){
-		System.out.println("See you later CEG 4110!");
+		System.out.println("These snozzberries taste like snozzberries");
 
 }
 }
